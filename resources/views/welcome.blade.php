5e911a608f689aacf5dd198e833677c74fe57a3f
vagrant <vagrant@homestead>
2017-02-22 05:42:25 +0000

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My Laravel
+                    My Fucking Laravel
                 </div>
 
                 <div class="links">
